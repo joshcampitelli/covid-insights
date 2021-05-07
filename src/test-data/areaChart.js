6ca2f6c64f2167c4d2bbdfd7762699e80e9b9052
joshcampitelli <joshcampitelli@cmail.carleton.ca>
2021-05-06 22:47:21 -0400

    Moved test data to separate files

---
+++ src/test-data/areaChart.js
@@ -0,0 +1,39 @@
+const AreaChartData = [
+    {
+        date: '2020-01',
+        supply: 4000,
+        demand: 2400
+    },
+    {
+        date: '2020-02',
+        supply: 3000,
+        demand: 1398
+    },
+    {
+        date: '2020-03',
+        supply: 2000,
+        demand: 9800
+    },
+    {
+        date: '2020-04',
+        supply: 2780,
+        demand: 3908
+    },
+    {
+        date: '2020-05',
+        supply: 1890,
+        demand: 4800
+    },
+    {
+        date: '2020-06',
+        supply: 2390,
+        demand: 3800
+    },
+    {
+        date: '2020-07',
+        supply: 3490,
+        demand: 4300
+    },
+];
+
+export default AreaChartData;
---
+++ src/test-data/barChart.js
@@ -0,0 +1,39 @@
+const BarChartData = [
+    {
+        name: 'Masks',
+        supply: 4000,
+        demand: 2400
+    },
+    {
+        name: 'Hand Sanitizer',
+        supply: 3000,
+        demand: 1398
+    },
+    {
+        name: 'Gloves',
+        supply: 2000,
+        demand: 9800
+    },
+    {
+        name: 'Gowns',
+        supply: 2780,
+        demand: 3908
+    },
+    {
+        name: 'Face Shields',
+        supply: 1890,
+        demand: 4800
+    },
+    {
+        name: 'Wipes',
+        supply: 2390,
+        demand: 3800
+    },
+    {
+        name: 'Respirators',
+        supply: 3490,
+        demand: 4300
+    }
+];
+
+export default BarChartData;
