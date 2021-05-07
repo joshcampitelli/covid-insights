79eb62f4b295419481be54fa855f3e67688b39d6
joshcampitelli <joshcampitelli@cmail.carleton.ca>
2021-05-07 17:31:01 -0400

    Mix of test / live data for Area chart for testing

--- src/test-data/areaChart.js
+++ src/test-data/areaChart.js
@@ -1,39 +1,628 @@
 const AreaChartData = [
     {
-        date: '2020-01',
-        supply: 4000,
-        demand: 2400
+        date: 'Jan 1 ',
+        vaccines: 6208,
+        cases: 17000
     },
     {
-        date: '2020-02',
-        supply: 3000,
-        demand: 1398
+        date: 'Jan 2 ',
+        vaccines: 4942,
+        cases: 17000
     },
     {
-        date: '2020-03',
-        supply: 2000,
-        demand: 9800
+        date: 'Jan 3 ',
+        vaccines: 5302,
+        cases: 17000
     },
     {
-        date: '2020-04',
-        supply: 2780,
-        demand: 3908
+        date: 'Jan 4 ',
+        vaccines: 23701,
+        cases: 17000
     },
     {
-        date: '2020-05',
-        supply: 1890,
-        demand: 4800
+        date: 'Jan 5 ',
+        vaccines: 17462,
+        cases: 17000
     },
     {
-        date: '2020-06',
-        supply: 2390,
-        demand: 3800
+        date: 'Jan 6 ',
+        vaccines: 28163,
+        cases: 17000
     },
     {
-        date: '2020-07',
-        supply: 3490,
-        demand: 4300
+        date: 'Jan 7 ',
+        vaccines: 36086,
+        cases: 17000
     },
+    {
+        date: 'Jan 8 ',
+        vaccines: 41208,
+        cases: 17000
+    },
+    {
+        date: 'Jan 9 ',
+        vaccines: 36086,
+        cases: 17000
+    },
+    {
+        date: 'Jan 10',
+        vaccines: 35587,
+        cases: 17000
+    },
+    {
+        date: 'Jan 11',
+        vaccines: 23362,
+        cases: 17000
+    },
+    {
+        date: 'Jan 12',
+        vaccines: 36086,
+        cases: 17000
+    },
+    {
+        date: 'Jan 13',
+        vaccines: 30716,
+        cases: 17000
+    },
+    {
+        date: 'Jan 14',
+        vaccines: 40283,
+        cases: 17000
+    },
+    {
+        date: 'Jan 15',
+        vaccines: 48195,
+        cases: 17000
+    },
+    {
+        date: 'Jan 16',
+        vaccines: 35604,
+        cases: 17000
+    },
+    {
+        date: 'Jan 17',
+        vaccines: 27451,
+        cases: 17000
+    },
+    {
+        date: 'Jan 18',
+        vaccines: 43924,
+        cases: 17000
+    },
+    {
+        date: 'Jan 19',
+        vaccines: 38987,
+        cases: 17000
+    },
+    {
+        date: 'Jan 20',
+        vaccines: 42589,
+        cases: 17000
+    },
+    {
+        date: 'Jan 21',
+        vaccines: 42622,
+        cases: 17000
+    },
+    {
+        date: 'Jan 22',
+        vaccines: 37742,
+        cases: 17000
+    },
+    {
+        date: 'Jan 23',
+        vaccines: 24632,
+        cases: 17000
+    },
+    {
+        date: 'Jan 24',
+        vaccines: 15213,
+        cases: 17000
+    },
+    {
+        date: 'Jan 25',
+        vaccines: 23489,
+        cases: 17000
+    },
+    {
+        date: 'Jan 26',
+        vaccines: 28505,
+        cases: 17000
+    },
+    {
+        date: 'Jan 27',
+        vaccines: 22870,
+        cases: 17000
+    },
+    {
+        date: 'Jan 28',
+        vaccines: 24703,
+        cases: 17000
+    },
+    {
+        date: 'Jan 29',
+        vaccines: 21172,
+        cases: 17000
+    },
+    {
+        date: 'Jan 30',
+        vaccines: 15013,
+        cases: 17000
+    },
+    {
+        date: 'Feb 1', 
+        vaccines: 18263,
+        cases: 17000
+    },
+    {
+        date: 'Feb 2', 
+        vaccines: 12710,
+        cases: 17000
+    },
+    {
+        date: 'Feb 3', 
+        vaccines: 13266,
+        cases: 17000
+    },
+    {
+        date: 'Feb 4', 
+        vaccines: 18598,
+        cases: 17000
+    },
+    {
+        date: 'Feb 5', 
+        vaccines: 22835,
+        cases: 17000
+    },
+    {
+        date: 'Feb 6', 
+        vaccines: 21469,
+        cases: 17000
+    },
+    {
+        date: 'Feb 7', 
+        vaccines: 12927,
+        cases: 17000
+    },
+    {
+        date: 'Feb 8', 
+        vaccines: 21449,
+        cases: 17000
+    },
+    {
+        date: 'Feb 9', 
+        vaccines: 25771,
+        cases: 17000
+    },
+    {
+        date: 'Feb 10', 
+        vaccines: 29760,
+        cases: 17000
+    },
+    {
+        date: 'Feb 11', 
+        vaccines: 32412,
+        cases: 17000
+    },
+    {
+        date: 'Feb 12', 
+        vaccines: 35150,
+        cases: 17000
+    },
+    {
+        date: 'Feb 13', 
+        vaccines: 33237,
+        cases: 17000
+    },
+    {
+        date: 'Feb 14', 
+        vaccines: 17824,
+        cases: 17000
+    },
+    {
+        date: 'Feb 15', 
+        vaccines: 9861,
+        cases: 17000
+    },{
+        date: 'Feb 16', 
+        vaccines: 26157,
+        cases: 17000
+    },
+    {
+        date: 'Feb 17', 
+        vaccines: 20480,
+        cases: 17000
+    },
+    {
+        date: 'Feb 18', 
+        vaccines: 27025,
+        cases: 17000
+    },
+    {
+        date: 'Feb 19', 
+        vaccines: 47850,
+        cases: 17000
+    },
+    {
+        date: 'Feb 20', 
+        vaccines: 49707,
+        cases: 17000
+    },
+    {
+        date: 'Feb 21', 
+        vaccines: 42397,
+        cases: 17000
+    },
+    {
+        date: 'Feb 22', 
+        vaccines: 61733,
+        cases: 17000
+    },
+    {
+        date: 'Feb 23', 
+        vaccines: 48362,
+        cases: 17000
+    },
+    {
+        date: 'Feb 24', 
+        vaccines: 46090,
+        cases: 17000
+    },
+    {
+        date: 'Feb 25', 
+        vaccines: 58893,
+        cases: 17000
+    },
+    {
+        date: 'Feb 26', 
+        vaccines: 67251,
+        cases: 17000
+    },
+    {
+        date: 'Feb 27', 
+        vaccines: 61729,
+        cases: 17000
+    },
+    {
+        date: 'Feb 28', 
+        vaccines: 46624,
+        cases: 17000
+    },
+    {
+        date: 'Mar 1',
+        vaccines: 59141,
+        cases: 17000
+    },
+    {
+        date: 'Mar 2',
+        vaccines: 72038,
+        cases: 17000
+    },
+    {
+        date: 'Mar 3',
+        vaccines: 73462,
+        cases: 17000
+    },
+    {
+        date: 'Mar 4',
+        vaccines: 77717,
+        cases: 17000
+    },
+    {
+        date: 'Mar 5',
+        vaccines: 85757,
+        cases: 17000
+    },
+    {
+        date: 'Mar 6',
+        vaccines: 74448,
+        cases: 17000
+    },
+    {
+        date: 'Mar 7',
+        vaccines: 57567,
+        cases: 17000
+    },
+    {
+        date: 'Mar 8',
+        vaccines: 86679,
+        cases: 17000
+    },
+    {
+        date: 'Mar 9',
+        vaccines: 69385,
+        cases: 17000
+    },
+    {
+        date: 'Mar 10',
+        vaccines: 78036,
+        cases: 17000
+    },
+    {
+        date: 'Mar 11',
+        vaccines: 98028,
+        cases: 17000
+    },
+    {
+        date: 'Mar 12',
+        vaccines: 110847,
+        cases: 17000
+    },
+    {
+        date: 'Mar 13',
+        vaccines: 103843,
+        cases: 17000
+    },
+    {
+        date: 'Mar 14',
+        vaccines: 92658,
+        cases: 17000
+    },
+    {
+        date: 'Mar 15',
+        vaccines: 124640,
+        cases: 17000
+    },
+    {
+        date: 'Mar 16',
+        vaccines: 120004,
+        cases: 17000
+    },
+    {
+        date: 'Mar 17',
+        vaccines: 138687,
+        cases: 17000
+    },
+    {
+        date: 'Mar 18',
+        vaccines: 156332,
+        cases: 17000
+    },
+    {
+        date: 'Mar 19',
+        vaccines: 162984,
+        cases: 17000
+    },
+    {
+        date: 'Mar 20',
+        vaccines: 133373,
+        cases: 17000
+    },
+    {
+        date: 'Mar 21',
+        vaccines: 91600,
+        cases: 17000
+    },
+    {
+        date: 'Mar 22',
+        vaccines: 146273,
+        cases: 17000
+    },
+    {
+        date: 'Mar 23',
+        vaccines: 122714,
+        cases: 17000
+    },
+    {
+        date: 'Mar 24',
+        vaccines: 170604,
+        cases: 17000
+    },
+    {
+        date: 'Mar 25',
+        vaccines: 190074,
+        cases: 17000
+    },
+    {
+        date: 'Mar 26',
+        vaccines: 216981,
+        cases: 17000
+    },
+    {
+        date: 'Mar 27',
+        vaccines: 166566,
+        cases: 17000
+    },
+    {
+        date: 'Mar 28',
+        vaccines: 146438,
+        cases: 17000
+    },
+    {
+        date: 'Mar 29',
+        vaccines: 187299,
+        cases: 17000
+    },
+    {
+        date: 'Mar 30',
+        vaccines: 169650,
+        cases: 17000
+    },
+    {
+        date: 'Mar 31',
+        vaccines: 219496,
+        cases: 17000
+    },
+    {
+        date: 'Apr 1',
+        vaccines: 206465,
+        cases: 17000
+    },
+    {
+        date: 'Apr 2',
+        vaccines: 160245,
+        cases: 17000
+    },
+    {
+        date: 'Apr 3',
+        vaccines: 200913,
+        cases: 17000
+    },
+    {
+        date: 'Apr 4',
+        vaccines: 114107,
+        cases: 17000
+    },
+    {
+        date: 'Apr 5',
+        vaccines: 183279,
+        cases: 17000
+    },
+    {
+        date: 'Apr 6',
+        vaccines: 203228,
+        cases: 17000
+    },
+    {
+        date: 'Apr 7',
+        vaccines: 231527,
+        cases: 17000
+    },
+    {
+        date: 'Apr 8',
+        vaccines: 262416,
+        cases: 17000
+    },
+    {
+        date: 'Apr 9',
+        vaccines: 316761,
+        cases: 17000
+    },
+    {
+        date: 'Apr 10',
+        vaccines: 245427,
+        cases: 17000
+    },
+    {
+        date: 'Apr 11',
+        vaccines: 181374,
+        cases: 17000
+    },
+    {
+        date: 'Apr 12',
+        vaccines: 334776,
+        cases: 17000
+    },
+    {
+        date: 'Apr 13',
+        vaccines: 270443,
+        cases: 17000
+    },
+    {
+        date: 'Apr 14',
+        vaccines: 297534,
+        cases: 17000
+    },
+    {
+        date: 'Apr 15',
+        vaccines: 306442,
+        cases: 17000
+    },
+    {
+        date: 'Apr 16',
+        vaccines: 320425,
+        cases: 17000
+    },
+    {
+        date: 'Apr 17',
+        vaccines: 254177,
+        cases: 17000
+    },
+    {
+        date: 'Apr 18',
+        vaccines: 200717,
+        cases: 17000
+    },
+    {
+        date: 'Apr 19',
+        vaccines: 262792,
+        cases: 17000
+    },
+    {
+        date: 'Apr 20',
+        vaccines: 238000,
+        cases: 17000
+    },
+    {
+        date: 'Apr 21',
+        vaccines: 316712,
+        cases: 17000
+    },
+    {
+        date: 'Apr 22',
+        vaccines: 360468,
+        cases: 17000
+    },
+    {
+        date: 'Apr 23',
+        vaccines: 368302,
+        cases: 17000
+    },
+    {
+        date: 'Apr 24',
+        vaccines: 286263,
+        cases: 17000
+    },
+    {
+        date: 'Apr 25',
+        vaccines: 231840,
+        cases: 17000
+    },
+    {
+        date: 'Apr 26',
+        vaccines: 262744,
+        cases: 17000
+    },
+    {
+        date: 'Apr 27',
+        vaccines: 256540,
+        cases: 17000
+    },
+    {
+        date: 'Apr 28',
+        vaccines: 261941,
+        cases: 17000
+    },
+    {
+        date: 'Apr 29',
+        vaccines: 305605,
+        cases: 17000
+    },
+    {
+        date: 'Apr 30',
+        vaccines: 288327,
+        cases: 17000
+    },
+    {
+        date: 'May 1',
+        vaccines: 232177,
+        cases: 17000
+    },
+    {
+        date: 'May 2',
+        vaccines: 173101,
+        cases: 17000
+    },
+    {
+        date: 'May 3',
+        vaccines: 226014,
+        cases: 17000
+    },
+    {
+        date: 'May 4',
+        vaccines: 232744,
+        cases: 17000
+    },
+    {
+        date: 'May 5',
+        vaccines: 283833,
+        cases: 17000
+    },
+    {
+        date: 'May 6',
+        vaccines: 356627,
+        cases: 17000
+    }
 ];
 
 export default AreaChartData;
