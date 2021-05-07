af7196b3cd68d0bbbfe243baef885a5718e5b1ed
joshcampitelli <joshcampitelli@cmail.carleton.ca>
2021-05-07 17:36:25 -0400

    Make chart display size uniform

--- src/components/graphs/SimpleAreaChart.js
+++ src/components/graphs/SimpleAreaChart.js
@@ -1,28 +1,39 @@
 import React from 'react';
 import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+    graph: {
+        display: 'flex',
+        flex: 8
+    }
+});
 
 const SimpleAreaChart = ({ data }) => {
+    const classes = useStyles();
+
     return (
-        <ResponsiveContainer width='100%' height='75%'>
+        <ResponsiveContainer className={classes.graph} height={'100%'} width={'100%'}>
             <AreaChart width={375} height={250} data={data}
-                margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
+                margin={{ top: 10, right: 10, left: 10, bottom: 0 }}>
                 <defs>
-                    <linearGradient id='colorUv' x1='0' y1='0' x2='0' y2='1'>
+                    <linearGradient id='colorVacc' x1='0' y1='0' x2='0' y2='1'>
                         <stop offset='5%' stopColor='#006064' stopOpacity={0.8} />
                         <stop offset='95%' stopColor='#006064' stopOpacity={0} />
                     </linearGradient>
-                    <linearGradient id='colorPv' x1='0' y1='0' x2='0' y2='1'>
+                    <linearGradient id='colorCases' x1='0' y1='0' x2='0' y2='1'>
                         <stop offset='5%' stopColor='#D41D2E' stopOpacity={0.8} />
                         <stop offset='95%' stopColor='#D41D2E' stopOpacity={0} />
                     </linearGradient>
                 </defs>
                 <Legend />
                 <XAxis dataKey='date' />
-                <YAxis />
+                <YAxis yAxisId='left' />
+                <YAxis yAxisId='right' orientation='right' />
                 <CartesianGrid strokeDasharray='3 3' vertical={false} />
                 <Tooltip />
-                <Area type='monotone' dataKey='supply' stroke='#D41D2E' fillOpacity={1} fill='url(#colorPv)' />
-                <Area type='monotone' dataKey='demand' stroke='#006064' fillOpacity={1} fill='url(#colorUv)' />
+                <Area yAxisId='right' type='monotone' dataKey='cases' stroke='#D41D2E' fillOpacity={1} fill='url(#colorCases)' />
+                <Area yAxisId='left'  type='monotone' dataKey='vaccines' stroke='#006064' fillOpacity={1} fill='url(#colorVacc)' />
             </AreaChart>
         </ResponsiveContainer>
     );
--- src/components/graphs/SimpleBarChart.js
+++ src/components/graphs/SimpleBarChart.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+    graph: {
+        display: 'flex',
+        flex: 8
+    }
+});
 
 const SimpleBarChart = ({ data }) => {
+    const classes = useStyles();
+
     return (
-        <ResponsiveContainer width='100%' height='75%'>
-            <BarChart width={730} height={250} data={data}>
+        <ResponsiveContainer className={classes.graph} height={'100%'} width={'100%'}>
+            <BarChart width={375} height={250} data={data}
+                margin={{ top: 10, right: 10, left: 10, bottom: 0 }}>
                 <defs>
                     <linearGradient id='colorSupply' x1='0' y1='0' x2='0' y2='1'>
                         <stop offset='5%' stopColor='#D41D2E' stopOpacity={0.8} />
@@ -15,13 +26,13 @@ const SimpleBarChart = ({ data }) => {
                         <stop offset='98%' stopColor='#006064' stopOpacity={0.5} />
                     </linearGradient>
                 </defs>
-                <CartesianGrid strokeDasharray='3 3' vertical={false} />
-                <XAxis dataKey='name' />
-                <YAxis />
-                <Tooltip cursor={{ fill: '#303030' }} />
                 <Legend />
-                <Bar dataKey='supply' fill='url(#colorSupply)' background={false} />
-                <Bar dataKey='demand' fill='url(#colorDemand)' background={false} />
+                <XAxis dataKey='name'/>
+                <YAxis />
+                <CartesianGrid strokeDasharray='3 3' vertical={false} />
+                <Tooltip cursor={{ fill: '#303030' }} />
+                <Bar type='monotone' dataKey='supply' stroke='#D41D2E' fillOpacity={1} fill='url(#colorSupply)'/>
+                <Bar type='monotone' dataKey='demand' stroke='#006064' fillOpacity={1} fill='url(#colorDemand)'/>
             </BarChart>
         </ResponsiveContainer>
     );
